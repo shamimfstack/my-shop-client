4713139ab4c3737ba9b5a0c093d9c08a73011626
shamimfstack <shamimfstack@gmail.com>
2024-04-22 19:22:13 +0600

    firebase configuration added

--- src/Shared/Navbar.jsx
+++ src/Shared/Navbar.jsx
@@ -2,7 +2,7 @@ import { Link, NavLink } from "react-router-dom";
 
 export default function Navbar() {
   return (
-    <div className="py-5">
+    <div className="py-5 bg-gray-200 px-6">
       <nav className="flex justify-between items-center">
         <Link to="/">Logo</Link>
 
